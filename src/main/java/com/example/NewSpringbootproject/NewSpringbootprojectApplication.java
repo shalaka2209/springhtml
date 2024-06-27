1d8cd94bfec4a0d9696a762d97f14d5c203fb61c
Shalaka <mandeshalaka6701@gmail.com>
2024-06-27 19:30:48 +0530

    first commit

--- src/main/java/com/example/NewSpringbootproject/NewSpringbootprojectApplication.java
+++ src/main/java/com/example/NewSpringbootproject/NewSpringbootprojectApplication.java
@@ -1,6 +1,6 @@
 package com.example.NewSpringbootproject;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@Controller
+
 
 public class NewSpringbootprojectApplication {
 	@GetMapping("/msg1")
